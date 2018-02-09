4e5867e1b85e73b0aadb01e2da3ced22eaac556d
Annie Polish <annie.polish@yale.edu>
2018-02-09 11:47:41 -0500

    account creation test

---
+++ spec/features/user_creates_account_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+#Capybara.default_driver = :selenium
+
+feature "User creates account" do
+  scenario "successfully" do
+    visit root_url
+    click_on "Sign up"
+    fill_in "Name", with: "example"
+    fill_in "Email", with: "example@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirmation", with: "password"
+    click_on "Create my account"
+    visit root_url
+    expect(page).to have_content "Task List"
+    expect(page).to have_content "Account"
+  end
+end
