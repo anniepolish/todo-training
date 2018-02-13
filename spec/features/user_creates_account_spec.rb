402eb7ee5022d1f417625e5f842b49efa69cb55b
Annie Polish <annie.polish@yale.edu>
2018-02-13 17:13:25 -0500

    poltergeist works, database cleaner doesnt

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -4,14 +4,14 @@ require "rails_helper"
 
 feature "User creates account", js: true do
   scenario "successfully" do
-    visit '/'
+    visit 'http://127.0.0.1:3000/'
     click_on "Sign up"
     fill_in "Name", with: "example"
     fill_in "Email", with: "example@example.com"
     fill_in "Password", with: "password"
     fill_in "Confirmation", with: "password"
     click_on "Create my account"
-    visit root_url
+    click_on "Home"
     expect(page).to have_content "Task List"
     expect(page).to have_content "Account"
   end
